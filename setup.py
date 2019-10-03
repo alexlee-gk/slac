5f0ab840486abb601fda68ef96d0dab2086f1301
Alex Lee <alexleegk@gmail.com>
2019-10-03 10:08:07 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='slac',
+      version='0.1',
+      description='Stochastic Latent Actor-Critic',
+      url='https://github.com/alexlee-gk/slac',
+      author='Alex Lee',
+      author_email='alexleegk@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
